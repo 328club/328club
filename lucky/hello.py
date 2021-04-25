67057348548e7466728c2c1e0bb7b9ddf87ce714
王翔 <xiang.wang@qcast.cn>
2021-04-25 18:14:15 +0800

    dantuo玩法

--- lucky/hello.py
+++ lucky/hello.py
@@ -38,13 +38,72 @@ def create_lucky(seed):
 	lottery=str(redball_pool+blueball_pool).replace('[','').replace(']','\n')
 	return lottery
 
+def create_dantuo(seed):
+	LUCKY_SEED=int(seed)
+	redball=list(range(1,36))
+	blueball=list(range(1,13))
+	redball_left=35
+	blueball_left=12
+	lucky_ball=0
+	redballdan_pool=[]
+	redballtuo_pool=[]
+	blueballdan_pool=[]
+	blueballtuo_pool=[]
+	random.shuffle(redball)
+	random.shuffle(blueball)
+	for i in range(4):
+		time.sleep(random.randint(1,LUCKY_SEED)/LUCKY_SEED)
+		for j in range(random.randint(1,LUCKY_SEED)%redball_left):
+			random.shuffle(redball)
+		lucky_ball=redball.pop()
+		# print("LUCKY_BALL is %s" %(lucky_ball))
+		redballdan_pool.append(lucky_ball)
+		redball_left=redball_left-1
+	for i in range(7):
+		time.sleep(random.randint(1,LUCKY_SEED)/LUCKY_SEED)
+		for j in range(random.randint(1,LUCKY_SEED)%redball_left):
+			random.shuffle(redball)
+		lucky_ball=redball.pop()
+		# print("LUCKY_BALL is %s" %(lucky_ball))
+		redballtuo_pool.append(lucky_ball)
+		redball_left=redball_left-1
+	# print("BLUE BALL IS ROLLING")
+	for i in range(1):
+		time.sleep(random.randint(1,LUCKY_SEED)/LUCKY_SEED)
+		for j in range(random.randint(1,LUCKY_SEED)%blueball_left):
+			# print(blueball)
+			random.shuffle(blueball)
+		# print(blueball)
+		lucky_ball=blueball.pop()
+		# print("LUCKY_BALL is %s" %(lucky_ball))
+		blueballdan_pool.append(lucky_ball)
+		blueball_left=blueball_left-1
+	for i in range(2):
+		time.sleep(random.randint(1,LUCKY_SEED)/LUCKY_SEED)
+		for j in range(random.randint(1,LUCKY_SEED)%blueball_left):
+			# print(blueball)
+			random.shuffle(blueball)
+		# print(blueball)
+		lucky_ball=blueball.pop()
+		# print("LUCKY_BALL is %s" %(lucky_ball))
+		blueballtuo_pool.append(lucky_ball)
+		blueball_left=blueball_left-1
+	redballdan_pool.sort()	
+	redballtuo_pool.sort()	
+	blueballdan_pool.sort()
+	blueballtuo_pool.sort()
+	lottery=str(redballdan_pool)+'+'+str(redballtuo_pool)+'    '+str(blueballdan_pool)+'+'+str(blueballtuo_pool)
+	return lottery
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-# print(hello_world())
 
 @app.route('/8888/')
 @app.route('/8888/<name>')
-def hello(name=None):
+def single(name=None):
     return render_template('hello.html', name=create_lucky(name))
+@app.route('/8848/')
+@app.route('/8848/<name>')
+def dantuo(name=None):
+    return render_template('hello.html', name=create_dantuo(name))
